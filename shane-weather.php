9b66a31008fd04960e0ecc0dbd49883696231fd4
Shane Sarkhot <shane@immedia.co.za>
2026-04-09 13:21:01 +0200

    Add initial WordPress plugin file

---
+++ shane-weather.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Shane Weather
+ * Description: A custom WordPress weather plugin for learning plugin development.
+ * Version: 1.0.0
+ * Author: Shane Sarkhot
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
